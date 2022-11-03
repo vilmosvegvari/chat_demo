ce6a9c44bcf5da5930dd75bac2f5ccda4285b896
Vilmos Vegvari <vilmos.vegvari@urss.hu>
2022-11-03 22:41:50 +0100

    Initial commit

---
+++ src/test/java/io/github/vilmosvegvari/chat_demo/ChatDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.vilmosvegvari.chat_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
